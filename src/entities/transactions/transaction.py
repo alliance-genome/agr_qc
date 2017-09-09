cf0f95daf7c1b8bef11ab1048a891323e2d7be0d
Chris Tabone <ctabone@morgan.harvard.edu>
2017-09-08 20:55:21 -0400

    Progress with Adam & Chris.

--- src/entities/transactions/transaction.py
+++ src/entities/transactions/transaction.py
@@ -2,13 +2,18 @@ import time
 
 class Transaction(object):
 
-	def __init__(self, graph):
-		self.graph = graph
+    def __init__(self, graph):
+        self.graph = graph
 
-	def deleteTransaction(self, query):
-		start = time.time()
-		with self.graph.session() as session:
-			with session.begin_transaction() as tx:
-				tx.run(query)
-		end = time.time()
-		print("Processed entries in %s s" % (end - start))
+    def deleteTransaction(self, query):
+        result = None
+        start = time.time()
+        with self.graph.session() as session:
+            # with session.begin_transaction() as tx:
+            result = session.run(query)
+        end = time.time()
+
+        for record in result:
+            print("%s %s" % (record['label'], record['primaryKey']))
+
+        print("Processed entries in %s s" % (end - start))
