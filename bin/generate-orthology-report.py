b59f519aff682f2be8c662c6420b9338b2586b9a
Ubuntu <ubuntu@ip-172-31-13-34.us-west-2.compute.internal>
2018-07-17 21:26:13 +0000

    adding changes / header to orthology file

--- bin/generate-orthology-report.py
+++ bin/generate-orthology-report.py
@@ -1,5 +1,9 @@
 import sys
 
+from dateutil.parser import parse
+from datetime import datetime
+from time import gmtime, strftime
+
 from neo4j.v1 import GraphDatabase
 
 # USAGE python bin/generateorthology-report.py <output-file>
@@ -8,17 +12,42 @@ uri = "bolt://localhost:7687"
 driver = GraphDatabase.driver(uri)
 
 outputFile = sys.argv[1]
+databaseVersion = sys.argv[2]
 
 with driver.session() as session:
 
     orthology_file = open(outputFile,'w')
 
-    orthology_file.write( "gene1\tgene2\talgorithms\talgorithmsMatch\toutOfAlgorithms\tisBestScore\tisBestRevScore\n")
+    datetimeNow = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    
+    header = """#########################################################################
+#
+# Ortholog File
+# Source: Alliance of Genomic Resources (Alliance)
+# Filter: stringent
+# Filter Details:
+#   Ortholog needs to be called by at least 3 Algorithms
+#        or is being called by either ZFIN or HGNC algorithms
+#        and either is best score or is best reverse score
+#   or ortholog is called by 2 algorithms and is either best score or best reverse score
+# Datebase Version: {databaseVersion}
+# Date: {datetimeNow}
+#
+#########################################################################
+""".format(datetimeNow = datetimeNow, databaseVersion=databaseVersion)
+
+    orthology_file.write(header)
+   
+#                                     AND gene2.modGlobalId = "FB:FBgn0036980"
+#                                      gene1.taxonId = "NCBITaxon:9606"
+
+    orthology_file.write( "Gene1ID\tGene1Symbol\tGene2ID\tGene2Symbol\tAlgorithms\tAlgorithmsMatch\tOutOfAlgorithms\tIsBestScore\tIsBestRevScore\n")
  
+    orthologList = set()
+
     with session.begin_transaction() as tx:
         for record in tx.run("""MATCH (gene1:Gene)-[o:ORTHOLOGOUS]->(gene2:Gene)
-                                WHERE gene1.taxonId = "NCBITaxon:9606"
-                                      AND (o.isBestScore = True OR o.isBestRevScore = True)
+                                WHERE (o.isBestScore = True OR o.isBestRevScore = True)
                                 WITH gene1, gene2, o
                                 MATCH (algorithm:OrthoAlgorithm)-[m:MATCHED]-(ogj:OrthologyGeneJoin)-[association:ASSOCIATION]-(gene1)
                                 WITH algorithm, ogj, gene1, gene2, o,count(DISTINCT algorithm.name) as numAlgorithm
@@ -29,16 +58,38 @@ with driver.session() as session:
                                       OR (numAlgorithm = 2 AND o.isBestScore = True and o.isBestRevScore = True)
                                 OPTIONAL MATCH (algorithm2:OrthoAlgorithm)-[m2:MATCHED]-(ogj2:OrthologyGeneJoin)-[association2:ASSOCIATION]-(gene1)
                                 WHERE ogj2.primaryKey = o.primaryKey
-                                OPTIONAL MATCH (algorithm3:OrthoAlgorithm)-[]-(ogj3:OrthologyGeneJoin)-[ASSOCIATION]-(gene1)
+                                OPTIONAL MATCH (algorithm3:OrthoAlgorithm)-[m3:NOT_CALLED]-(ogj3:OrthologyGeneJoin)-[ASSOCIATION]-(gene1)
                                 WHERE ogj3.primaryKey = o.primaryKey
-                                RETURN gene1.modGlobalId as Gene1,gene2.modGlobalId as Gene2, collect(algorithm2.name) as algorithms,count(DISTINCT algorithm2.name) as numAlgorithmMatch, count(DISTINCT algorithm3) as numAlgorithm, toString(o.isBestScore) as best,toString(o.isBestRevScore) as bestRev"""):
+                                RETURN gene1.primaryKey AS gene1ID,
+                                       gene1.symbol AS gene1Symbol,
+                                       gene2.primaryKey AS gene2ID,
+                                       gene2.symbol AS gene2Symbol,
+                                       collect(algorithm2.name) as algorithms,
+                                       count(DISTINCT algorithm2.name) as numAlgorithmMatch,
+                                       count(DISTINCT algorithm3.name) as numAlgorithmNotCalled,
+                                       toString(o.isBestScore) as best,
+                                       toString(o.isBestRevScore) as bestRev"""):
             algorithms = "|".join(set(record["algorithms"]))
             numAlgorithmMatch = str(record["numAlgorithmMatch"])
-            numAlgorithm = str(record["numAlgorithm"])
-            gene1 = record["Gene1"]
-            gene2 = record["Gene2"]
+            numAlgorithm = 12 - record["numAlgorithmNotCalled"]
+            numAlgorithm = str(numAlgorithm)
             best = record["best"]
             bestRev = record["bestRev"]
-            orthology_file.write("\t".join([gene1,gene2,algorithms,numAlgorithmMatch,numAlgorithm,best,bestRev]) + "\n")
+            matchStr = record["gene2ID"] + "-" + record["gene1ID"]
+
+            if matchStr in orthologList: continue
+            
+            revMatchStr = record["gene1ID"] + "-" + record["gene2ID"] 
+            orthologList.add(revMatchStr)
+
+            orthology_file.write("\t".join([record["gene1ID"],
+                                            record["gene1Symbol"],
+                                            record["gene2ID"],
+                                            record["gene2Symbol"],
+                                            algorithms,
+                                            numAlgorithmMatch,
+                                            numAlgorithm,
+                                            best,
+                                            bestRev]) + "\n")
 
     orthology_file.close()
