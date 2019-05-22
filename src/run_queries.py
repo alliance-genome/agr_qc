95c2e026d1c32a5d2fcb7b631fc49aeb27724200
Adam Wright <adam.j.wright82@gmail.com>
2019-05-22 13:52:55 -0400

    adding defaults to port and host for neo4j

--- src/run_queries.py
+++ src/run_queries.py
@@ -1,8 +1,17 @@
 from aggregate_updater import AggregateUpdater
 import os
 
-host = os.environ['NEO4J_HOST']
-port = os.environ['NEO4J_PORT']
+
+if "NEO4J_HOST" in os.environ:
+    host = int(os.environ['NEO4J_HOST'])
+else:
+    host = "localhost"
+
+if "NEO4J_PORT" in os.environ:
+    port = int(os.environ['NEO4J_PORT'])
+else:
+    port = 7687
+
 uri = "bolt://" + host + ":" + port
 
 if __name__ == '__main__':
